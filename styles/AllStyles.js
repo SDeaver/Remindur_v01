bf97a133091cc91f55ee405a1dd2ba51c98bd86c
Stephen Deaver <stephendeaver@gmail.com>
2024-07-26 17:46:35 -0500

    initial commit
    Initial commit with nothing created yet.

---
+++ styles/AllStyles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+
+export const colors = {
+   bg: 'pink',
+   text: 'black',
+}
+
+
+
+export const allStyles = StyleSheet.create ({
+
+   mainContainer: {
+      backgroundColor: colors.bg,
+      height: '100%',
+   },
+
+});
+
