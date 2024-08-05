aec8c30f6b74d23d693fd6857d6f47399110fe9c
Stephen Deaver <stephendeaver@gmail.com>
2024-08-05 12:52:21 -0500

    testing removed

--- App.js
+++ App.js
@@ -6,29 +6,14 @@ import EventCard from './components/EventCard';
 import MonthTitle from './components/MonthTitle';
 import YearBreak from './components/YearBreak';
 import { allStyles } from './styles/AllStyles';
-import { testData } from './components/TestData';
 
 
 export default function App() {
    
-   const [sortedData, setSortedData] = useState(null);
+   const [sortedData, setSortedData] = useState(initData);
    const [hiddenMonths, setHiddenMonths] = useState([]);
    const [toggleToForceFlatListRender, setToggleToForceFlastListRender] = useState(true);
 
-   // useEffect (() => {
-   
-   //    if (sortedData === null) {
-   //       let newDataArray = initData();
-   //       setSortedData(newDataArray);
-   //    }     
-      
-   // }, [])
-
-   function initFunction() {
-
-      setSortedData(initData());   
-  
-   }
    
    function displayNextEvent(item) {
 
@@ -54,7 +39,7 @@ export default function App() {
    function toggleHideMonth(month) {
 
       let newHiddenArray = hiddenMonths;                                            
-      let position = newHiddenArray.indexOf(month);
+      let position = hiddenMonths.indexOf(month);
 
       if (position === -1) {
          newHiddenArray.push(month);
@@ -64,27 +49,11 @@ export default function App() {
       }
 
       setHiddenMonths(newHiddenArray);
-      //forceFlatListRender();
+      forceFlatListRender();
    }
 
    function forceFlatListRender() {
-      setToggleToForceFlastListRender(!toggleToForceFlatListRender);
-   }
-
-   function testFunction() {
-      let newTestArray = sortedData;
-
-      //newTestArray.reverse();
-      newTestArray.pop();
-      //sortedData.reverse();
-      
-      setSortedData(newTestArray);
-      forceFlatListRender();
-      
-      //setSortedData(testData);
-    
-      //setSortedData(sortedData);
-
+      setToggleToForceFlastListRender(!toggleToForceFlatListRender)
    }
 
    return (
@@ -92,26 +61,12 @@ export default function App() {
 
         <FlatList
             data={sortedData}
+            //extraData={hiddenMonths}
             style={allStyles.listContainer}
             renderItem={({ item }) => (
                displayNextEvent(item)
             )}
          />
-
-         <View style={allStyles.buttonView}>
-            <Pressable style={allStyles.initButton} onPressOut={initFunction}>
-               <Text>INIT</Text>
-            </Pressable>
-
-            <Pressable style={allStyles.initButton} onPressOut={testFunction}>
-               <Text>TEST</Text>
-            </Pressable>
-
-            <Pressable style={allStyles.initButton} onPressOut={forceFlatListRender}>
-               <Text>FORCE</Text>
-            </Pressable>
-         </View>
-
 
       </Pressable>
       
